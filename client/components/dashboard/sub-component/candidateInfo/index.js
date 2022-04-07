2ca120fd36388a0bd2e5cc30c5fc11e7e5f00c53
NiteshTiwari147 <64365692+NiteshTiwari147@users.noreply.github.com>
2022-04-07 22:42:12 +0530

    added male profile pic

--- client/components/dashboard/sub-component/candidateInfo/index.js
+++ client/components/dashboard/sub-component/candidateInfo/index.js
@@ -12,12 +12,15 @@ class CandidateInfo extends Component {
             },
             {
                 link: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1160&q=80"
+            },
+            {
+                link: 'https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png'
             }
         ]
         const {firstName, lastName, currentEmploymnt} = this.props
         return (
             <div className='basicInfoContainer shadow'>
-                <img className="profilePic"src={url[0].link} alt="Avatar"/>
+                <img className="profilePic"src={url[2].link} alt="Avatar"/>
                 <h5 className="profileName">{firstName} {lastName}</h5>
                 <text className="profilePosition">{currentEmploymnt.designation} at {currentEmploymnt.company}</text>
                 <button className="viewPortfolioBtn">View Portfolio</button>
